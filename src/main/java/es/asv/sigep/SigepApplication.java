513577ef2a32ed259018e8e0dc7400ce362b942e
AdrianSVHappy <adriansuarezvaldayo573@gmail.com>
2025-04-15 13:07:05 +0200

    Spring Initializr

---
+++ src/main/java/es/asv/sigep/SigepApplication.java
@@ -0,0 +1,15 @@
+package es.asv.sigep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigepApplication {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hola¿");
+		SpringApplication.run(SigepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/asv/sigep/SigepApplicationTests.java
@@ -0,0 +1,13 @@
+package es.asv.sigep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
